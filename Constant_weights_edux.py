e6d436ca59fc9e9a2fdb0c223e138cda3d84009f
Dmitri Volchek <dvolchek@mail.ru>
2017-11-25 14:41:47 +0300

    decision maker

---
+++ Constant_weights_edux.py
@@ -0,0 +1,27 @@
+import numpy as np
+import math
+
+direction = 0.0
+level = 0.0
+competency = 1.0
+
+weight_d = 0.6
+weight_l = 0.6
+weight_c = 0.3
+
+
+def activation(x):
+	if x>=0.5:
+		return True
+	else:
+		return False
+
+def sigmoid(x):
+	return(1/(1+math.exp(-x)))  
+
+weights = np.array([weight_d, weight_l, weight_c])
+inputs = np.array([direction, level, competency])
+x = np.dot(inputs, weights)
+
+decision = activation(x)
+print ("Result: " + str(decision))
